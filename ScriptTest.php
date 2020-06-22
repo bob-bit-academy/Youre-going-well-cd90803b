8ceb5fe51e59e550c981d8cd7dac5f5332713f0e
bob-bit-academy <53262971+bob-bit-academy@users.noreply.github.com>
2020-06-22 07:41:36 +0200

    Update ScriptTest.php

--- ScriptTest.php
+++ ScriptTest.php
@@ -1,11 +1,2 @@
 <?php
-// @codingStandardsIgnoreStart
-    echo"hi";
-    $a = 2;
-// @codingStandardsIgnoreEnd
-if(2==$a){echo "ho";}
-// @codingStandardsIgnoreLine
-if($a=3){echo"beep";}
-if($a==2)
-    echo "hi";
-exit (1);
+echo "HAllo wereld!";
